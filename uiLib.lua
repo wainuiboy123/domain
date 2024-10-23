7ff30e1df10322211e99e59f4104d196614ee5f2
Domain <119091875+wainuiboy123@users.noreply.github.com>
2024-10-24 10:53:43 +1300

    Update uiLib.lua

--- uiLib.lua
+++ uiLib.lua
@@ -1,7 +1,7 @@
+local plrs = game.Players
+local plr = plrs.LocalPlayer
+local plrui = plr.PlayerGui
 
-
-
-
-
-
-
+local sui = Instance.new("ScreenGui")
+sui.Parent = plrui
+sui.Name = "TestSuccess"
