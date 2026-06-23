25c63e46e14cd49ff7303378f25f78d10ee1853f
a <119091875+wainuiboy123@users.noreply.github.com>
2026-06-24 10:12:35 +1200

    Update mainScript1.lua

--- mainScript1.lua
+++ mainScript1.lua
@@ -1,24 +1,28 @@
 local im = peripheral.find("inventory_manager")
 local cb = peripheral.find("chat_box")
 
-if not im then error("inventory manager not found") end
+if not im then error("inventory_manager not found") end
 if not cb then error("chat_box not found") end
 
-print("Running dumper")
+print("Ready")
 
 while true do
-    local e, username, msg = os.pullEvent("chat")
+    local _, username, msg = os.pullEvent("chat")
 
-    if msg == "dump" and username == "wainuiomata" then
+    if msg == "dump" and username == "toastonrye" then
         print("Dumping inventory slots 9-26")
 
         local items = im.getItems()
 
         for _, item in pairs(items) do
             if item.slot >= 9 and item.slot <= 26 then
-                pcall(function()
+                local ok, err = pcall(function()
                     im.removeItemFromPlayer(item, item.count)
                 end)
+
+                if not ok then
+                    print("Failed:", err)
+                end
             end
         end
 
