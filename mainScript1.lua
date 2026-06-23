cc53a9594667c9abcae036bdf5f03b0a238fec58
a <119091875+wainuiboy123@users.noreply.github.com>
2026-06-24 10:02:12 +1200

    Update mainScript1.lua

--- mainScript1.lua
+++ mainScript1.lua
@@ -9,7 +9,7 @@ print("Running dumper")
 while true do
     local e, username, msg = os.pullEvent("chat")
 
-    if msg == "dump" and username == "toastonrye" then
+    if msg == "dump" and username == "wainuiomata" then
         print("Dumping inventory slots 9-26")
 
         local items = im.getItems()
